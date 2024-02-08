94595e14afa3ddad98ddc1356409bf5e5baf4d26
AnneEberhard <anne.eberhard@gmx.net>
2024-02-08 11:32:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+async function init() {
+    await includeHTML();
+  }
+
+async function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        await fetch(file)
+          .then(response => response.text())
+          .then(data => {
+            elmnt.innerHTML = data;
+            elmnt.removeAttribute("w3-include-html");
+          })
+          .catch(error => {
+            console.error(`Error fetching HTML: ${error}`);
+          });
+      }
+    }
+  }
+  
